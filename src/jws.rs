4efb7d6de90e4b69f74a36143419ae7310333786
William Brown <william@firstyear.id.au>
2026-05-21 14:04:28 +1000

    Revert "Add OAuth2 ClientID to protected headers (#50)"
    This reverts commit 95a3eb9cbc9e9c3272a18ed243ee75d08c291f4e.

--- src/jws.rs
+++ src/jws.rs
@@ -84,12 +84,6 @@ impl JwsBuilder {
         self
     }
 
-    /// Set the OAuth2 Client ID
-    pub fn set_client_id(mut self, client_id: Option<&str>) -> Self {
-        self.header.client_id = client_id.map(|s| s.to_string());
-        self
-    }
-
     /// Finalise this builder
     pub fn build(self) -> Jws {
         let JwsBuilder { header, payload } = self;
@@ -108,11 +102,6 @@ impl Jws {
     /// Get the bytes of the payload of this JWS
     pub fn payload(&self) -> &[u8] {
         &self.payload
-    }
-
-    /// View the authenticated header content
-    pub fn header(&self) -> &ProtectedHeader {
-        &self.header
     }
 
     /// Create a JWS from a serialisable type. This assumes you want to encode
